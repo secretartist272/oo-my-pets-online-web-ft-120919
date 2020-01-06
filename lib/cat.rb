2b205d6e68308247b88b8702681a842aef11a088
Jacqueline Freeland <secretartist272@gmail.com>
2020-01-06 02:02:27 +0000

    Automatically backed up by Learn

--- lib/cat.rb
+++ lib/cat.rb
@@ -1,3 +1,29 @@
+require 'pry'
+
 class Cat
-  # code goes here
+  
+  attr_reader :name, :owner
+  
+  @@all = []
+   
+  def initialize(name, owner)
+    @name = name
+    @owner = owner
+    @@all << self
+  end
+  
+  def owner
+    @owner = owner
+  end
+  
+  def new_owner
+    owner.cats 
+  end
+  
+  
+  def self.all
+    @@all
+  end
+  
+  
 end
--- lib/owner.rb
+++ lib/owner.rb
@@ -1,3 +1,48 @@
+require 'pry'
+
 class Owner
-  # code goes here
+  attr_reader :species, :name
+  
+  
+  @@all = []
+  
+  counter = 0
+  
+  def initialize(name)
+    @name = name
+    @@all << self
+  end
+  
+  def initalizes(species)
+    @species = species
+  end
+  
+  def species
+     @species ="human"
+  end
+  
+  def say_species
+    "I am a #{species}."
+  end
+  
+  def self.all
+    @@all 
+  end
+   
+  def self.count
+    @@all.count
+  end
+  
+  def self.reset_all
+    self.all.clear
+  end
+  
+  def buy_cat
+    Cat.new(name, self)
+  end
+  
+  def buy_dog
+    Dog.new(name, self)
+  end
+  
 end
